e592e3df3a4453dfa03f034f4b33b674bb7726b1
TEJAS RAJENDRA THONGE <80546718+tejasthonge@users.noreply.github.com>
2024-07-07 10:56:52 +0530

    Tab bar and tab conttoreller added

--- multivendor/lib/vendors/views/screens/nav_screens/upload_screen.dart
+++ multivendor/lib/vendors/views/screens/nav_screens/upload_screen.dart
@@ -5,10 +5,39 @@ class VendorUploadScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-
-      body: Center(
-        child: Text('Upload screen'),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold( 
+        appBar: AppBar( 
+          bottom: TabBar(
+            tabs: [ 
+              Tab( 
+                child: Text( 
+                  "General"
+                ),
+              ),
+              Tab( 
+                child: Text( 
+                  "Shipping"
+                ),
+              ),
+              Tab( 
+                child: Text( 
+                  "Attributes"
+                ),
+              ),
+              Tab( 
+                child: Text( 
+                  "Images"
+                ),
+              ),
+              
+            ],
+           ),
+        ),
+        body: Center(
+          child: Text('Upload screen'),
+        ),
       ),
     );
   }
